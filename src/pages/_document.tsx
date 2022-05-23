3b42aedc0f33bb86e13dd6fab3003193b253e3a2
Tiny <405401372@gapp.fju.edu.tw>
2022-05-24 01:54:03 +0800

    add gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,26 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname
+                  ${(process.env.NODE_ENV === 'development' && ",debug_event: 1,traffic_type : 'internal'") || ''}
+                });
+              `
+            }}
+          />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
